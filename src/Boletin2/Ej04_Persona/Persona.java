6feb23eeab04148a6ca48a6c43b516b6cf80c048
Pablo Álvarez Calvo <a21pabloac1@ATEIS.LOCAL>
2023-05-03 21:16:14 +0200

    Wip

---
+++ src/Boletin2/Ej04_Persona/Persona.java
@@ -0,0 +1,30 @@
+package Boletin2.Ej04_Persona;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+    private String nombre;
+
+    public Persona(){
+
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+}
