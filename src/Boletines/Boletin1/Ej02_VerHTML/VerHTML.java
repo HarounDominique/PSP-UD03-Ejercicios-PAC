c290b9a864d2625895369e7aceca11a9503fe083
HAROUN <haroundominique@gmail.com>
2023-05-24 00:00:49 +0200

    añadiendo en ejercicio VerHTML un ejemplo de URLConection

--- src/Boletines/Boletin1/Ej02_VerHTML/VerHTML.java
+++ src/Boletines/Boletin1/Ej02_VerHTML/VerHTML.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 
 public class VerHTML {
@@ -17,7 +18,13 @@ public class VerHTML {
 
         try {
             url = new URL(sc.nextLine());
+            /*
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String inputLine;
+
+             */
+            URLConnection urlCon = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
             String inputLine;
 
             int urlPort = url.getPort();
